a3b23227114a5ec8c4ffa77a783505b30d523039
MichaelHochriegl <michael@hochriegl.dev>
2023-06-05 18:03:54 +0200

    add test for `HubClientAttribute`

---
+++ tests/SignalRGen.Generator.Tests/HubClientAttributeTests.Generates_HubClientAttribute_Correctly#HubClientAttribute.g.verified.cs
@@ -0,0 +1,7 @@
+﻿//HintName: HubClientAttribute.g.cs
+namespace SignalRGen.Generator;
+[System.AttributeUsage(System.AttributeTargets.Interface)]
+public sealed class HubClientAttribute : System.Attribute
+{
+    public string? HubName;
+}
