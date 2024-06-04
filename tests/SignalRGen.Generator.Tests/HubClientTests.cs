2da78001f5ac111197ac1e6a9b63856a376da9f9
Michael Hochriegl <michael@hochriegl.dev>
2024-06-04 15:35:13 +0200

    Add generation support for nested Namespaces
    The SignalRClientGenerator function was updated by changing the GetInterfaceNamespace input parameter from `InterfaceDeclarationSyntax` to `ISymbol`. This improves the calculation of namespace for generated hub clients.
    
    In addition, NamespaceNested end-to-end tests were added to validate the correct generation of hub clients nested within namespaces. Other dependent classes were modified to support these changes.

--- tests/SignalRGen.Generator.Tests/HubClientTests.cs
+++ tests/SignalRGen.Generator.Tests/HubClientTests.cs
@@ -34,4 +34,39 @@ public class HubClientTests
 
         return TestHelper.Verify(source, true);
     }
+    
+    [Fact]
+    public Task Generates_NamespaceNested_HubClient_Correctly()
+    {
+        // Arrange
+        const string source = """
+                              using SignalRGen.Generator;
+                              using SignalRGen.Generator.Tests.TestData;
+
+                              namespace SignalRGen.Clients {
+                                    namespace Nested {
+                                          [HubClient(HubName = "TestHubClient", HubUri = "examples")]
+                                          public interface ITestHubClient
+                                          {
+                                            Task ReceiveCustomTypeUpdate(IEnumerable<CustomTypeDto> customTypes);
+                                            Task ReceiveFooUpdate(string bar, int bass);
+                                            
+                                            [ServerToClientMethod]  
+                                            Task ReceiveNormalTypeWithSpecificAttributeApplied(string bazz, int buzz);
+                                            
+                                            [NoOpTest]
+                                            Task ReceiveWithArbitraryAttribute(int blub);
+                                            
+                                            [ClientToServerMethod]
+                                            Task SendClientToServerNoReturnType(string rick, int age);
+                                            
+                                            [ClientToServerMethod]
+                                            Task<string> SendClientToServerWithReturnType(string morty, bool partOfMission);
+                                          }
+                                    }
+                              }
+                              """;
+
+        return TestHelper.Verify(source, true);
+    }
 }
