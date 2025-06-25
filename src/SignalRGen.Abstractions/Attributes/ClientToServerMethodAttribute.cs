a7014e21af4510283ac4eb5be6bdc6599e0943b8
MichaelHochriegl <michael@hochriegl.dev>
2025-06-25 14:57:57 +0200

    Remove unused ClientToServerMethodAttribute and ServerToClientMethodAttribute classes
    Deleted the obsolete attributes as they are no longer required for SignalR generation, simplifying the codebase.

--- src/SignalRGen.Abstractions/Attributes/ClientToServerMethodAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRGen.Abstractions.Attributes;
-
-/// <summary>
-/// An attribute used to designate a method as a client-to-server method in the context of SignalR communications.
-/// </summary>
-[AttributeUsage(AttributeTargets.Method)]
-public sealed class ClientToServerMethodAttribute : Attribute
-{
-}
