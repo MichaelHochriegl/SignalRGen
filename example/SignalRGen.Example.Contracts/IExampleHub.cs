275b34ace9bfb627f95be77343ba52c91c4b825b
MichaelHochriegl <michael@hochriegl.dev>
2025-06-29 16:30:53 +0200

    Remove debug methods for Analyzer and CodeFix check

--- example/SignalRGen.Example.Contracts/IExampleHub.cs
+++ example/SignalRGen.Example.Contracts/IExampleHub.cs
@@ -6,14 +6,11 @@ namespace SignalRGen.Example.Contracts;
 [HubClient(HubUri = "example")]
 public interface IExampleHub : IBidirectionalHub<IExampleHubServerToClient, IExampleHubClientToServer>
 {
-    Task FooBar();
-    Task Foo1();
 }
 
 [HubClient(HubUri = "example-server-to-client-only")]
 public interface IExampleServerToClientOnlyHub : IServerToClientHub<IExampleHubServerToClient>
 {
-    Task Bar();
 }
 
 public record MyCustomType(string Hey, int Dude);
--- example/SignalRGen.Example.Contracts/IExampleHubServerToClient.cs
+++ example/SignalRGen.Example.Contracts/IExampleHubServerToClient.cs
@@ -3,8 +3,4 @@ namespace SignalRGen.Example.Contracts;
 public interface IExampleHubServerToClient
 {
     Task ReceiveExampleCountUpdate(int count);
-    Task<string> IllegalReturnType();
-    Task<string> GetMessage();     // ❌ Error + Quick Fix available
-    Task<int> GetCount();
-
 }
