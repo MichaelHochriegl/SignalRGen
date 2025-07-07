0ef7bddf399e5488511d5b6a5d1a2c51e5ea5e11
Dr_Cox1911 <78933445+MichaelHochriegl@users.noreply.github.com>
2025-07-07 15:52:04 +0200

    Add basic SignalR chat implementation (#83)
    Added a basic chat to better showcase SignalR communication with a generated client from SignalRGen.

--- example/SignalRGen.Example.Client.BlazorServer/Program.cs
+++ example/SignalRGen.Example.Client.BlazorServer/Program.cs
@@ -4,7 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddSignalRHubs(c => c.HubBaseUri = new Uri("http://localhost:5155")).WithExampleHubClient();
+// We register the generated SignalR client here.
+// These calls allow for a whole suite of configuration values, but we are mostly happy with the defaults.
+// Notice that we can chain the generated Client configurations as done with the `ExampleHubClient`
+// and the `ChatHubContractClient`.
+builder.Services
+    .AddSignalRHubs(c => c.HubBaseUri = new Uri("http://localhost:5155"))
+    .WithExampleHubClient()
+    .WithChatHubContractClient(config =>
+    {
+        // We overwrite the default lifetime for the ChatHubContractClient from `Singleton` to `Scoped`,
+        // as each tab will have its own instance of the client.
+        config.HubClientLifetime = ServiceLifetime.Scoped;
+    });
 
 var app = builder.Build();
 
