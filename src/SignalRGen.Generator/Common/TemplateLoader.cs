0d45d3e181557e5323219f6180d4934b314b4da6
MichaelHochriegl <michael@hochriegl.dev>
2025-10-16 20:52:34 +0200

    Integrate Scriban for template loading and replace inlined HubClientBase template with embedded resource

---
+++ src/SignalRGen.Generator/Common/TemplateLoader.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using Scriban;
+
+namespace SignalRGen.Generator.Common;
+
+/// <summary>
+/// Provides methods for loading and parsing embedded Scriban templates.
+/// </summary>
+internal static class TemplateLoader
+{
+    /// <summary>
+    /// Retrieves an embedded Scriban template by its name, parses it, and returns the loaded template object.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the template to load. This should correspond to the resource name
+    /// without the file extension, located in the 'SignalRGen.Generator.Sources' namespace.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Template"/> object that represents the parsed Scriban template.
+    /// </returns>
+    public static Template GetTemplate(string name)
+    {
+        using var stream = Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream(
+                $"SignalRGen.Generator.Sources.{name}.sbntxt"
+            )!;
+
+        using var reader = new StreamReader(stream);
+        return Template.Parse(reader.ReadToEnd());
+    }
+}
